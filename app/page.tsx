5ba76e45e590258202a14dce72b63ec2288be051
Rachouan Rejeb <info@rachouan.com>
2023-01-24 16:35:40 +0000

    Add custom font

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <Section className="flex justify-center items-center h-4/5">
       <div className="container flex flex-col gap-y-6 justify-center items-center text-center">
-        <h1 className="text-4xl sm:text-6xl font-semibold">
+        <h1 className="font-roc text-4xl sm:text-6xl font-semibold">
           <Balancer>
             A designer, developer, and illustrator from Belgium.
           </Balancer>
